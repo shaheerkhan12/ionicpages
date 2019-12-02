d52a7ea98bb63f0a3ad87fdb772767c3135bf571
shaheerkhan58 <shaheerkhan58@hotmail.com>
2019-12-02 11:49:00 +0500

    design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , AngularFontAwesomeModule],
   providers: [
     StatusBar,
     SplashScreen,
